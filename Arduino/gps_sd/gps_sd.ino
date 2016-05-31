099f7f7e17cb30993653d2af2d2fdb048753b076
Gom_Dzhabbar <gd_mops@mail.ru>
2016-06-01 00:34:52 +0400

    test tone and led

--- Arduino/gps_sd/gps_sd.ino
+++ Arduino/gps_sd/gps_sd.ino
@@ -9,7 +9,7 @@
 
 TinyGPS gps;
 SoftwareSerial ss(5, 3);
-static void print_float(float val, float invalid, int len, int prec);
+// static void print_float(float val, float invalid, int len, int prec);
 
 boolean menu = false, select = false, clear = true;
 int mode = 0, mode_menu = 0;
@@ -25,33 +25,33 @@ int lcd_key     = 0;
 int adc_key_in  = 0;
 int countDot = 1;
 int delayValue = 1000;
-int notaPin = 11;
+int notaPin = 6;
 
 File dataFile;
 
-#define btnRIGHT  0
-#define btnUP     1
-#define btnDOWN   2
-#define btnLEFT   3
-#define btnSELECT 4
-#define btnNONE   5
+// #define btnRIGHT  0
+// #define btnUP     1
+// #define btnDOWN   2
+// #define btnLEFT   3
+// #define btnSELECT 4
+// #define btnNONE   5
 
 boolean displayOn = true;
  
-int read_LCD_buttons(){
-    adc_key_in = analogRead(0); 
+// int read_LCD_buttons(){
+//     adc_key_in = analogRead(0); 
 
-    if (adc_key_in > 1000) return btnNONE; 
+//     if (adc_key_in > 1000) return btnNONE; 
 
-    // For V1.1 us this threshold
-    if (adc_key_in < 50)   return btnRIGHT;  
-    if (adc_key_in < 250)  return btnUP; 
-    if (adc_key_in < 450)  return btnDOWN; 
-    if (adc_key_in < 650)  return btnLEFT; 
-    if (adc_key_in < 850)  return btnSELECT;  
+//     // For V1.1 us this threshold
+//     if (adc_key_in < 50)   return btnRIGHT;  
+//     if (adc_key_in < 250)  return btnUP; 
+//     if (adc_key_in < 450)  return btnDOWN; 
+//     if (adc_key_in < 650)  return btnLEFT; 
+//     if (adc_key_in < 850)  return btnSELECT;  
 
-    return btnNONE;
-}
+//     return btnNONE;
+// }
 void oneTone(int nota, int longTime, int itemPin) {
     tone(notaPin, nota);
     delay(longTime);
@@ -72,30 +72,35 @@ void setup(){
 
   Serial.println("start");
 
-	// // pinMode(bee, OUTPUT);
 	pinMode(0, INPUT);
 	pinMode(1, INPUT);
-	// pinMode(12, OUTPUT);
+  // pinMode(10, OUTPUT);  
+  // pinMode(8, OUTPUT);  
 
 	lcd.begin();
 	lcd.backlight();
 	lcd.setCursor(0,0); 
 
-  setTone(1023,200,2);
-  // noTone(11);
-  // tone(11, 1000);
-  // delay(200);
-  // noTone(11);
-
-
 	if (!SD.begin(4))
 	lcd.println("Card failed  ");
 	else
 	lcd.println("card init  ");
+  setTone(800, 200, 1);
 	delay(3000);
 }
 
 void loop(){
+
+  // digitalWrite(10, LOW); 
+  // delay(100);
+  // digitalWrite(8, HIGH); 
+
+  // setTone(800, 200, 1);
+  // delay(1000);
+  // setTone(150, 200, 1);
+  // delay(1000);
+  // setTone(600, 200, 1);
+  // delay(1000);
 
   char sz[32];
 
@@ -119,10 +124,14 @@ void loop(){
   }    
 
   if (flat == TinyGPS::GPS_INVALID_F_ANGLE){
+    
     if(displayOn)
   	 lcd.print("gps error");
+
      Serial.println("gps error");
      delayValue = 1000;
+
+     // setTone(150, 200, 1);
   } else {
     clear = false;
 
@@ -231,60 +240,60 @@ static void smartdelay(unsigned long ms)
   } while (millis() - start < ms);
 }
 
-static void print_float(float val, float invalid, int len, int prec)
-{
-  if (val == invalid)
-  {
-    while (len-- > 1)
-      Serial.print('*');
-    Serial.print(' ');
-  }
-  else
-  {
-    // Serial.print(val, prec);
-    int vi = abs((int)val);
-    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
-    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
-    for (int i=flen; i<len; ++i)
-      Serial.print(' ');
-  }
-  smartdelay(0);
-}
+// static void print_float(float val, float invalid, int len, int prec)
+// {
+//   if (val == invalid)
+//   {
+//     while (len-- > 1)
+//       Serial.print('*');
+//     Serial.print(' ');
+//   }
+//   else
+//   {
+//     // Serial.print(val, prec);
+//     int vi = abs((int)val);
+//     int flen = prec + (val < 0.0 ? 2 : 1); // . and -
+//     flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
+//     for (int i=flen; i<len; ++i)
+//       Serial.print(' ');
+//   }
+//   smartdelay(0);
+// }
 
-static void print_date(TinyGPS &gps)
-{
-  int year;
-  byte month, day, hour, minute, second, hundredths;
-  unsigned long age;
-  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
-  if (age == TinyGPS::GPS_INVALID_AGE)
-    Serial.print("********** ******** ");
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
-        month, day, year, hour, minute, second);
-    // Serial.print(sz);
-  }
-  // print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
-  smartdelay(0);
-}
+// static void print_date(TinyGPS &gps)
+// {
+//   int year;
+//   byte month, day, hour, minute, second, hundredths;
+//   unsigned long age;
+//   gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
+//   if (age == TinyGPS::GPS_INVALID_AGE)
+//     Serial.print("********** ******** ");
+//   else
+//   {
+//     char sz[32];
+//     sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
+//         month, day, year, hour, minute, second);
+//     // Serial.print(sz);
+//   }
+//   // print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
+//   smartdelay(0);
+// }
 
-static void print_int(unsigned long val, unsigned long invalid, int len)
-{
-  char sz[32];
-  if (val == invalid)
-    strcpy(sz, "*******");
-  else
-    sprintf(sz, "%ld", val);
-  sz[len] = 0;
-  for (int i=strlen(sz); i<len; ++i)
-    sz[i] = ' ';
-  if (len > 0) 
-    sz[len-1] = ' ';
-  // Serial.print(sz);
-  smartdelay(0);
-}
+// static void print_int(unsigned long val, unsigned long invalid, int len)
+// {
+//   char sz[32];
+//   if (val == invalid)
+//     strcpy(sz, "*******");
+//   else
+//     sprintf(sz, "%ld", val);
+//   sz[len] = 0;
+//   for (int i=strlen(sz); i<len; ++i)
+//     sz[i] = ' ';
+//   if (len > 0) 
+//     sz[len-1] = ' ';
+//   // Serial.print(sz);
+//   smartdelay(0);
+// }
 /*
 1) добавить звуковой и световой сигнал ошибки от gps 
 2) добавить на отображение время. на дисплей, и в трек.
